8fb006afa830d548cd33382e6244aec65d055442
hpincket <harrisonpincket@gmail.com>
2015-07-13 14:03:31 -0400

    Can leave defs adjacent

--- vm_find_defs.sh
+++ vm_find_defs.sh
@@ -39,14 +39,22 @@ BEGIN {
         }
         VALUE = 0;
     } else if (NF==2) {
-        # If definition field
-        if (VALUE == 0) {
-            # If first line in a definition
-            WORD = $1;
-            DEF = $2;
-        } else if(WORD != "DECK"){
-            # If continuing line in a definition
-            DEF=DEF $2;
+        # If new definition
+        if ($1 != ""){
+          # Clean up old definition
+          if(VALUE == 1){
+            if(WORD == "DECK") {
+                DECK = DEF;
+            } else {
+                printf("%s\t%s\n",WORD,DEF) >> ("'$dest'/" DECK "-'$now'.tsv");
+            }
+            VALUE = 0;
+          }
+          # New Definition
+          WORD = $1;
+          DEF = $2;
+        } else if(VALUE == 1){
+          DEF=DEF $2;
         }
         VALUE = 1;
     }
